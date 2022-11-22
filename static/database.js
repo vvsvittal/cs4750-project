ddfffa6c25a1282e6cacb91cf5826fd21a07840c
Aneesh Vittal <akv6zr@virginia.edu>
2022-11-22 13:13:36 -0500

    nothing

--- static/database.js
+++ static/database.js
@@ -11,14 +11,6 @@ sql.connect(function(err){
     if (err) throw error;
     console.log("Connected to database");
 })
-// sql.query('SELECT * FROM Address', function(error, result){
-//     if (error)
-//         throw error;
-    
-//     result.forEach(r => {
-//         console.log(r);
-//     })
-// })
 
 module.exports ={
 closeConnection: function (){
